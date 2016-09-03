9283330b6e6273c67ae4f8835df1e7ccea7a892e
brandl3s <brandlescandles@gmail.com>
2016-09-03 23:17:51 +0100

    First
    the first draft/day’s work!

---
+++ player-saver.js
@@ -0,0 +1,28 @@
+/* ----- YOUTUBE -----*/
+//if this file is linked properly, then the console will print out "app.js ready to share documentaries"
+console.log("player.js ready to load the video page");
+
+        var player;
+        function onYouTubePlayerAPIReady() {
+            player = new YT.Player('player', {
+              height: '765',
+              width: '1010',
+              videoId: 'QIpTGbbqS0Q',
+              events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+              }
+            });
+        }
+
+        // autoplay video
+        function onPlayerReady(event) {
+            event.target.playVideo();
+        }
+
+        // when video ends
+        function onPlayerStateChange(event) {        
+            if(event.data === YT.PlayerState.PAUSED) {          
+                location.href = "../"
+            }
+        }
