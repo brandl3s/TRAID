6d6da07d29de986d6c4147f59d0365e249341bae
brandl3s <brandlescandles@gmail.com>
2016-09-09 12:01:14 +0100

    Added JS for Wufoo form
    linked JS and CSS to form index

---
+++ form.js
@@ -0,0 +1,20 @@
+//js for form
+console.log("app.js ready to display the form");
+
+var z3jxa1g12hidja;(function(d, t) {
+	var s = d.createElement(t), options = {
+	'userName':'traid',
+	'formHash':'z3jxa1g12hidja',
+	'autoResize':true,
+	'height':'437',
+	'async':true,
+	'host':'wufoo.com',
+	'header':'show',
+	'ssl':true};
+	s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+	s.onload = s.onreadystatechange = function() {
+	var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+	try { z3jxa1g12hidja = new WufooForm();z3jxa1g12hidja.initialize(options);z3jxa1g12hidja.display(); } catch (e) {}};
+	var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})
+(document, 'script');
