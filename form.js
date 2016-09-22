60e2c987c1f92a16894da4d83e7f75006a91eaa8
brandl3s <brandlescandles@gmail.com>
2016-09-22 11:32:12 +0100

    Form works & Centering WORKS YEA
    woohooo EXCEPT DUMMY submit button

--- form.js
+++ form.js
@@ -1,6 +1,10 @@
 //js for form
 console.log("app.js ready to display the form");
 
+var emailField = 'Field3';
+var reactionField = 'Field8';
+var commentField = 'Field2';
+
 // var z3jxa1g12hidja;(function(d, t) {
 // 	var s = d.createElement(t), options = {
 // 	'userName':'traid',
@@ -21,34 +25,38 @@ console.log("app.js ready to display the form");
 
 document.getElementById("send").addEventListener("click", sendData);
 
-	var reaction = {
-		document.getElementById('shocked').addEventListener('click', function() {
- 		reaction = 'shocked'; 
-		});
-		document.getElementById('sad').addEventListener('click', function() {
- 		reaction = 'sad'; 
-		});
-		document.getElementById('speechless').addEventListener('click', function() {
- 		reaction = 'speechless'; 
-		});
-		document.getElementById('angry').addEventListener('click', function() {
- 		reaction = 'angry'; 
-		});
-		document.getElementById('thoughtful').addEventListener('click', function() {
- 		reaction = 'thoughtful'; 
-		});
-	};
+var reaction = '';
+
+document.getElementById('shocked').addEventListener('click', function() {
+	  reaction = 'shocked'; 
+});
+document.getElementById('sad').addEventListener('click', function() {
+	  reaction = 'sad'; 
+});
+document.getElementById('speechless').addEventListener('click', function() {
+	  reaction = 'speechless'; 
+});
+document.getElementById('angry').addEventListener('click', function() {
+	  reaction = 'angry'; 
+});
+document.getElementById('thoughtful').addEventListener('click', function() {
+	  reaction = 'thoughtful'; 
+});
+
 
 function sendData() {
 
+
+  var message = "Sending, please wait";
+  document.getElementById("response").innerHTML = message;
+
     var data = {
-  		Field6 = reaction,
-		Field2: document.getElementById("comment").value,
-        Field3: document.getElementById("email").value
+  		Field8: reaction,
+		  Field2: document.getElementById("comment").value,
+      Field3: document.getElementById("email").value
     };
 
     console.log(data);
-
 
     //http://api.jquery.com/jQuery.ajax/
     $.ajax({
@@ -60,18 +68,24 @@ function sendData() {
       dataType: "json"
     })
   .done(function( msg ) {
-      var message = '';
-    if (msg.Success === 1) {
-        message = 'Thank you for your repsonse';
-        document.getElementById("success").style.display = "visible";
+    console.log(msg);
+     if (msg.Success === 1) {
+        message = document.write("<h1>Thank you</h1> <h2>How you help</h2> <p> Shopping at TRAID charity shops and donating unwanted clothes raises funds to support projects to improve the lives of garment workers globally.</p>");
     } else if (msg.Success === 0) {
-        message = msg.FieldErrors[0].ErrorText;
-        document.getElementById("failed").style.display = "block";
+        //figure out the error
+
+        if (msg.FieldErrors[0].ID === emailField) {
+          message = "Please provide a valid email address";          
+        } else {
+          message = msg.FieldErrors[0].ErrorText;          
+        }
+
+        // document.getElementById("failed").style.display = "block";
     } else {
         message = 'unknown error has occurred';
     }
 
-    document.getElementById('status').innerHTML = message;
+    document.getElementById("response").innerHTML = message;
 
   });
 };
