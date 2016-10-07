589b3bbad1e61f5a946ca0563431cb5940d53f2c
brandl3s <brandlescandles@gmail.com>
2016-10-07 17:37:36 +0100

    home page video editing

--- player.js
+++ player.js
@@ -2,33 +2,6 @@
 //if this file is linked properly, then the console will print out "app.js ready to share documentaries"
 console.log("player.js ready to load the video page");
 
-      var tag = document.createElement('script');
-
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-        var player;
-        function onYouTubeIframeAPIReady() {
-            player = new YT.Player('player', {
-              height: '765',
-              width: '1010',
-              videoId: 'QIpTGbbqS0Q',
-              events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-              }
-            });
-        }
-
-        // autoplay video
-        function onPlayerReady(event) {
-            event.target.playVideo();
-        }
-
-        // when video ends
-        function onPlayerStateChange(event) {        
-            if(event.data === YT.PlayerState.ENDED) {          
-                location.href = "reaction"
-            }
-        }
+document.getElementById('player').addEventListener('ended',function(){
+     window.location.href = "reaction";
+ },false);
