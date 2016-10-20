c17688a68a9ed341b4b0e0ada08983d84cd1025e
brandl3s <brandlescandles@gmail.com>
2016-10-20 15:11:07 +0100

    Deleted some useless files

--- show-hide.js
+++
@@ -1,7 +0,0 @@
-//js for show/hide
-
-console.log("app.js ready to display the success or failure");
-
-document.getElementById('success').style.display = "none";
-
-document.getElementById('failed').style.display = "none";
