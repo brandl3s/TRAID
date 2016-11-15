3a00a81ff194a5c5bd50c88eef2289e8d1b2b9e4
brandl3s <brandlescandles@gmail.com>
2016-11-15 20:10:13 +0000

    Changes with the help of Dan to JS, Cordova iPad tings

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 //if this file is linked properly, then the console will print out "app.js ready to share documentaries"
 console.log("app.js ready to share documentaries");
 
-document.getElementById("homeButton").onclick = function () {
-    location.href = "choose-a-film";
+var chooseAFilm = function() {
+  console.log('hello')
+  location.href = "choose-a-film";
 };
+
+var homeButton = document.getElementById("homeButton");
+homeButton.addEventListener('click', chooseAFilm);
+
+var anotherButton = document.getElementById("another-button");
+anotherButton.addEventListener('click', chooseAFilm);
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+       
+    }
+};
+
+app.initialize();
--- player.js
+++ player.js
@@ -1,9 +1,18 @@
 //if this file is linked properly, then the console will print out "app.js ready to share documentaries"
 console.log("player.js ready to load the video page");
 
-document.getElementById('player').addEventListener('ended',function(){
-     window.location.href = "..//reaction";
- },false);
+
+//listens for the end of a selected video
+var player = document.getElementById('player');
+if (player) {
+  player.addEventListener('ended',function(){
+       window.location.href = "../reaction";
+   }, false);
+}
+
+
+
+
 
  // -- Screen Saver Timer --
  var timeout;
